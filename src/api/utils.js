ba2c3ae9100801250f743877b821901d7acdb211
Anggy <anggydahianaruiz99@gmail.com>
2021-08-18 12:16:07 -0500

    const

--- src/api/utils.js
+++ src/api/utils.js
@@ -2,14 +2,14 @@ import tusDatos from './https-tusdatos';
 
 //Función de prueba para la conexion con tus datos
 const test = async() => {
-    var username = 'pruebas';
-    var password = 'password';
-    var basicAuth = 'Basic ' + btoa(username + ':' + password);
+    const username = 'pruebas';
+    const password = 'password';
+    const basicAuth = 'Basic ' + btoa(username + ':' + password);
     const rta = await tusDatos
         .post(
             '/api/launch', { "fechaE": "01/12/2017", "typedoc": "CC", "doc": 123 }, {
                 headers: {
-                    "Content-Type: application/json",
+                    "Content-Type": "application/json",
                     Authorization: basicAuth,
                 },
             }).catch((err) => {
