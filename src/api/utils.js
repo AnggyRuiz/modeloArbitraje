652d3ba97f52546e4e478b2aa2d1e580a8105f21
Anggy <anggydahianaruiz99@gmail.com>
2021-08-18 11:45:34 -0500

    https

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,22 +1,24 @@
-import axios from "axios";
+import tusDatos from './https-tusdatos';
 
 //Función de prueba para la conexion con tus datos
 const test = async() => {
-    var session_url = 'http://docs.tusdatos.co/api/launch';
     var username = 'pruebas';
     var password = 'password';
     var basicAuth = 'Basic ' + btoa(username + ':' + password);
-    axios.post(session_url, {
-        fechaE: "01/12/2017",
-        typedoc: "CC",
-        doc: 123,
-    }, {
-        headers: { 'Authorization': +basicAuth }
-    }).then((response) => {
-        console.log('Authenticated', response);
-    }).catch((error) => {
-        console.log('Error on Authentication', error);
-    });
+    const rta = await tusDatos
+        .post(
+            '/api/launch', { "fechaE": "01/12/2017", "typedoc": "CC", "doc": 123 }, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: basicAuth,
+                },
+            }).catch((err) => {
+            console.log(err);
+            return false;
+        });
+    console.log('rtaaaaaa', rta);
+    return rta ? true : false;
+
 }
 export default {
     test,
